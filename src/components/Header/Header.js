d2b791dacc55c8111e2e8656e8db8283cf697e4b
PanWidmo <tomasz.baranww@gmail.com>
2022-01-31 19:58:22 +0100

    Hiding Navigation in special case

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
+import logoImg from 'assets/images/logo.png';
+
+const Wrapper = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  width: 100%;
+`;
+
+const Navigation = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+
+  @media (max-width: 1200px) {
+    display: none;
+  }
+`;
+
+const RightNavigation = styled.div`
+  display: flex;
+  align-items: center;
+
+  a {
+    font-weight: 600;
+    margin-right: 10px;
+  }
+`;
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <Wrapper>
+      <a href="https://www.tesla.com">
+        <img src={logoImg} alt="Logo" />
+      </a>
+      <Navigation>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
+        <a href="#">Solar Roof</a>
+        <a href="#">Solar Panels</a>
+      </Navigation>
+      <RightNavigation>
+        <a href="#">Shop</a>
+        <a href="#">Account</a>
+        <a href="#">Menu</a>
+      </RightNavigation>
+    </Wrapper>
+  );
 };
 
 export default Header;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import Section from 'components/Section/Section';
 
-const Container = styled.div`
+const Wrapper = styled.div`
   height: 100vh;
 `;
 
 const Home = () => {
   return (
-    <Container>
+    <Wrapper>
       <Section title="Model 3" description="Order Online for Touchless Delivery" leftBtnText="Custom order" rightBtnText="Existing inventory" />
       <Section title="Model Y" description="Order Online for Touchless Delivery" leftBtnText="Custom order" rightBtnText="Existing inventory" />
       <Section title="Model S" description="Order Online for Touchless Delivery" leftBtnText="Custom order" rightBtnText="Existing inventory" />
@@ -16,7 +16,7 @@ const Home = () => {
       <Section title="Solar Panels" description="Lowest Cost Solar Panels in America" leftBtnText="Order now" rightBtnText="Learn more" />
       <Section title="Solar Roof" description="Produce Clean Energy From Your Roof" leftBtnText="Order now" rightBtnText="Learn more" />
       <Section title="Accessories" leftBtnText="Ship now" />
-    </Container>
+    </Wrapper>
   );
 };
 
